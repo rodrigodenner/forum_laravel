c4e2ec18e2438d93a84292667b355715df3f363a
souza-su <rodrigodenner.art@gmail.com>
2023-11-17 16:35:04 -0300

    Tailwind install

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title') - {{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <section class="container px-4 mx-auto">
