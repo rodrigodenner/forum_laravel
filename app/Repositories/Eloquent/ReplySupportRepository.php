a3eebfa730cb1a0903fa33900deb21039a83a92b
rodrigodenner <rodrigodenner.art@gmail.com>
2023-11-22 14:52:17 -0300

    resluçao de bug ReplySupportRepository

--- app/Repositories/Eloquent/ReplySupportRepository.php
+++ app/Repositories/Eloquent/ReplySupportRepository.php
@@ -3,15 +3,18 @@
 namespace App\Repositories\Eloquent;
 
 use App\DTO\Replies\CreateReplayDTO;
-use App\Models\ReplySupport;
+use App\Models\ReplySupport as Model;
 use App\Repositories\Contracts\ReplayRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 use stdClass;
 
 class ReplySupportRepository implements ReplayRepositoryInterface
 {
-  
-    public function __construct(ReplySupport $model){}
+
+  public function __construct(
+    protected Model $model,
+) {
+}
 
     public function getAllBySupportId(string $supportId): array
     {
