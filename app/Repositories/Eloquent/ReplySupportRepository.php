898e000f866314b96f6d41f0be73eb64824cdc5c
rodrigodenner <rodrigodenner.art@gmail.com>
2023-11-22 14:48:17 -0300

    update ReplayService

---
+++ app/Repositories/Eloquent/ReplySupportRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories\Contracts\Eloquent;
+
+use App\DTO\Replies\CreateReplayDTO;
+
+use App\Models\ReplySupport as Model;
+use App\Repositories\Contracts\ReplayRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+use stdClass;
+
+class ReplySupportRepository implements ReplayRepositoryInterface
+{
+    public function __construct(Model $model){}
+
+    public function getAllBySupportId(string $supportId): array
+    {
+      $replies = $this->model->where('support_id',$supportId)->get();
+
+      return $replies->toArray();
+    }
+
+    public function createNew(CreateReplayDTO $dto): stdClass
+    {
+      $replay = $this->model->create([
+        'content' => $dto->content,
+        'support_id' => $dto->supportId,
+        'user_id' => Auth::user()->id,
+      ]);
+    }
+}
